d8fc28013d7364ad1beac9c82823ea9e805020a9
korsikova <nastcka@gmail.com>
2018-08-27 18:28:59 +0300

    did scroll (js). started to do pop-up (js)

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,26 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menu = new Menu('menu__button', 'menu__list', 'menu__button_press_pressed', 'menu__list_visibility_visible');
-})
-
-
-
-function Menu(menuBtnClass, menuListClass, menuBtnPressClass, menuListVisibilityClass) {
-    var menuBtn = document.getElementsByClassName(menuBtnClass)[0],
-        menuList = document.getElementsByClassName(menuListClass)[0];
-
-    menuBtn.addEventListener('click', function () {
-        menuList.classList.toggle(menuListVisibilityClass);
-        menuBtn.classList.toggle(menuBtnPressClass);
-
-        event.stopPropagation();
-
-        document.addEventListener('click', function menuActivated () {
-            menuList.classList.remove(menuListVisibilityClass);
-            menuBtn.classList.remove(menuBtnPressClass);
-            document.removeEventListener('click', menuActivated);
-        });
-    });
-};
 
 
 var button = document.getElementsByClassName('header__button')[0];
