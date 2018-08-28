e5b7736b3eaf604ec0b0cf11fc553a96f94432f3
korsikova <nastcka@gmail.com>
2018-08-28 16:14:15 +0300

    changed scroll(js) and did pop-up(js)

--- app/js/pop-up.js
+++ app/js/pop-up.js
@@ -1,26 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menu = new Menu('menu__button', 'menu__list', 'menu__button_press_pressed', 'menu__list_visibility_visible');
-})
-
-
-
-function Menu(menuBtnClass, menuListClass, menuBtnPressClass, menuListVisibilityClass) {
-    var menuBtn = document.getElementsByClassName(menuBtnClass)[0],
-        menuList = document.getElementsByClassName(menuListClass)[0];
-
-    menuBtn.addEventListener('click', function () {
-        menuList.classList.toggle(menuListVisibilityClass);
-        menuBtn.classList.toggle(menuBtnPressClass);
-
-        event.stopPropagation();
-
-        document.addEventListener('click', function menuActivated () {
-            menuList.classList.remove(menuListVisibilityClass);
-            menuBtn.classList.remove(menuBtnPressClass);
-            document.removeEventListener('click', menuActivated);
-        });
-    });
-};
 var popUp = [];
 
 popUp[0] = {
@@ -149,24 +126,4 @@ function fulfil(i) {
             }
         }
     }
-}
-
-var button = document.getElementsByClassName('header__button')[0];
-var catalogue = document.getElementsByClassName('catalogue__title')[0];
-var point = catalogue.offsetTop;
-
-button.addEventListener('click', function() {
-    scrollTo(document.documentElement, point, 200)
-});
-
-function scrollTo(element, to, duration) {
-    if (duration <= 0) return;
-    var difference = to - element.scrollTop;
-    var perTick = difference / duration * 10;
-
-    setTimeout(function() {
-        element.scrollTop = element.scrollTop + perTick;
-        if (element.scrollTop === to) return;
-        scrollTo(element, to, duration - 10);
-    }, 10);
 }
--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,38 +1,20 @@
 
-
 var button = document.getElementsByClassName('header__button')[0];
 var catalogue = document.getElementsByClassName('catalogue__title')[0];
 var point = catalogue.offsetTop;
 
-button.addEventListener('click', function scroll() {
-    scrolling = window.pageYOffset;
-    winScroll();
+button.addEventListener('click', function() {
+    scrollTo(document.documentElement, point, 200)
 });
 
-function winScroll() {
-    window.scrollTo(0, scrolling);
-    scrolling += 10;
-    var timer = setTimeout(winScroll, 1);
-    if (scrolling >= point) {
-        clearTimeout(timer);
-    }
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
 }
-
-
-
-// button.addEventListener('click', function() {
-//     scrollTo(document.documentElement, point, 200)
-// });
-//
-// function scrollTo(element, to, duration) {
-//     console.log(point);
-//     if (duration <= 0) return;
-//     var difference = to - element.scrollTop;
-//     var perTick = difference / duration * 10;
-//
-//     setTimeout(function() {
-//         element.scrollTop = element.scrollTop + perTick;
-//         if (element.scrollTop === to) return;
-//         scrollTo(element, to, duration - 10);
-//     }, 10);
-// }
